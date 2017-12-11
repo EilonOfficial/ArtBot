1565eafaccd0ab0a7b20279ce7951e5d49cc5565
EilonOfficial <gamingbros975@gmail.com>
2017-12-11 18:42:23 -0500

    Update choosewinner.js

--- commands/main/choosewinner.js
+++ commands/main/choosewinner.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "Main",
-    password: "Bobber06",
+    password: "********",
     database: "Discord"
 })
 
